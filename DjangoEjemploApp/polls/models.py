524acee4f21167151c9c0feb7f67ad61a7fd5e62
pbermeo <pbermeo@consenso.com.ec>
2022-05-30 12:48:41 -0500

    Cambios Realizados Primer Curso

--- DjangoEjemploApp/polls/models.py
+++ DjangoEjemploApp/polls/models.py
@@ -1,3 +1,30 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Question(models.Model):
+    #id Django lo genera automaticamente no hace falta crearlo, y lo hace incrementable y primary
+    #question_text
+    # pub_date
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")#Le coloco un nombre entendible
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        #timedelta saca diferencia de tiempo, en este caso le digo que de un dia
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)#Le indico que el primer valor va a ser 0
+    def __str__(self):
+        return self.choice_text
+
+
--- DjangoEjemploApp/polls/views.py
+++ DjangoEjemploApp/polls/views.py
@@ -1,7 +1,76 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
+from .models import Question, Choice
+'''
 def index(req):
-    return HttpResponse("Mensaje Retorna")
+    latest_question_list = Question.objects.all()
+    return render(req, "polls/index.html", {
+        "latest_question_list":latest_question_list
+    })
+    #return HttpResponse("Estas en la pagina principal de Premios Platzi App")
+
+
+def detail(req, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    #question = Question.objects.get(pk = question_id)
+    return render(req, "polls/detail.html",{
+        "question": question
+    })
+    #return HttpResponse(f"Estas viendo la pregunta # {question_id}")
+
+
+def results(req, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(req, "polls/results.html",{
+        "question":question
+    })
+    #return HttpResponse(f"Estas viendo los resultados de la pregunta # {question_id}")
+'''
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        #Return the last five published question
+        #-pub_date "-" al inicio quiere decir que ordenare desde
+        # las mas recientes a las mas antiguas, si no pongo me trae
+        # ordenado de las mas viejas a las mas nuevas
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(req, question_id):
+    #question = Question.objects.get(pk=1)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_chocie = question.choice_set.get(pk = req.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(req, "polls/detail.html", {
+            "question": question,
+            "error_message": "No elegiste una respuesta"
+        })
+    else:
+        selected_chocie.votes += 1
+        selected_chocie.save()
+        return(HttpResponseRedirect(reverse("polls:results", args=(question.id,))))
+
+
+
+
+    #return HttpResponse(f"Estas votando a la pregunta # {question_id}")
+
